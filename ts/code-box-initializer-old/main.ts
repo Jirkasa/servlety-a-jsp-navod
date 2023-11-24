81e8fe4c2f239c28be493067d5b7b137fd5a04df
Jiří <satorajirka@seznam.cz>
2023-11-24 11:43:54 +0100

    remove old files for code boxes

--- ts/code-box-initializer-old/main.ts
+++
@@ -1,9 +0,0 @@
-import CodeBoxInitializer from "./CodeBoxInitializer";
-
-const codeBoxInitializer = new CodeBoxInitializer();
-
-function initCodeBoxes() : void {
-    codeBoxInitializer.init();
-}
-
-export default initCodeBoxes;
