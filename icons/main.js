05825b0920b5efbdb8caaeac9e5bdaa4f12a1005
Jiří <satorajirka@seznam.cz>
2024-01-17 19:39:05 +0100

    create home page

--- icons/main.js
+++ icons/main.js
@@ -2,6 +2,8 @@ import "./arrow-right.svg";
 import "./double-arrow-right.svg";
 import "./file.svg";
 import "./inventory.svg";
+import "./javatpoint-logo.svg";
 import "./key.svg";
 import "./opened-folder.svg";
 import "./package.svg";
+import "./tutorialspoint-logo.svg";
