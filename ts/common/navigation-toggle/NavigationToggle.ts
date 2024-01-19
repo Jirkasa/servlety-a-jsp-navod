50d431d68ccad04a5ccbc261c9242063f4269b8d
Jiří <satorajirka@seznam.cz>
2024-01-19 22:47:40 +0100

    comment code for Navigation and NavigationToggle

--- ts/common/navigation-toggle/NavigationToggle.ts
+++ ts/common/navigation-toggle/NavigationToggle.ts
@@ -1,11 +1,24 @@
 import Navigation from "./Navigation";
 
+/**
+ * Manages navigation toggle functionality.
+ */
 class NavigationToggle {
+    /** Determines whether navigations are opened. */
     private opened : boolean;
+    /** Navigations controlled by toggle. */
     private navigations : Array<Navigation>;
+    /** Toggle button. */
     private toggleButton : HTMLElement;
+    /** CSS class that is added to toggle button when navigations are opened. */
     private toggleButtonCheckedCSSClass : string;
 
+    /**
+     * Creates new navigation toggle.
+     * @param toggleButton Toggle button.
+     * @param toggleButtonCheckedCSSClass CSS class to be added to toggle button when navigations are opened.
+     * @param navigations Navigations to be controlled.
+     */
     constructor(toggleButton : HTMLElement, toggleButtonCheckedCSSClass : string, navigations : Array<Navigation>) {
         this.opened = false;
         this.navigations = [];
@@ -16,6 +29,7 @@ class NavigationToggle {
         this.toggleButton.addEventListener("click", () => this.onToggleButtonClick());
     }
 
+    /** Called when toggle button is clicked. */
     private onToggleButtonClick() {
         this.opened = !this.opened;
 
